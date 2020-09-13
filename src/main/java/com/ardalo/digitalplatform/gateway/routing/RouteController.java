6a444f75c0e9fb95e0c2382cbda77779132bd0f4
Torsten Blasche <torsten.blasche@obi.de>
2020-09-13 10:11:12 +0200

    Added route administration endpoints

--- src/main/java/com/ardalo/digitalplatform/gateway/routing/RouteController.java
+++ src/main/java/com/ardalo/digitalplatform/gateway/routing/RouteController.java
@@ -1,9 +1,13 @@
 package com.ardalo.digitalplatform.gateway.routing;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.cloud.gateway.actuate.GatewayControllerEndpoint;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +19,35 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/routes/v1")
 public class RouteController {
 
-  private final RouteService routeService;
+  private final GatewayControllerEndpoint gatewayControllerEndpoint;
 
   @Autowired
-  public RouteController(RouteService routeService) {
-    this.routeService = routeService;
+  public RouteController(GatewayControllerEndpoint gatewayControllerEndpoint) {
+    this.gatewayControllerEndpoint = gatewayControllerEndpoint;
   }
 
   @GetMapping
-  public Flux<Route> getRoutes() {
-    return this.routeService.findAll();
+  public Flux<Map<String, Object>> getRoutes() {
+    return this.gatewayControllerEndpoint.routes();
   }
 
-  @PostMapping
-  public ResponseEntity<Mono<Route>> createRoute(@RequestBody Route route) {
-    Mono<Route> createdRoute = this.routeService.createRoute(route);
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdRoute);
+  @GetMapping("/{routeId}")
+  public Mono<ResponseEntity<Map<String, Object>>> getRoute(@PathVariable String routeId) {
+    return this.gatewayControllerEndpoint.route(routeId);
+  }
+
+  @PostMapping("/{routeId}")
+  public Mono<ResponseEntity<Void>> saveRoute(@PathVariable String routeId, @RequestBody RouteDefinition route) {
+    return this.gatewayControllerEndpoint
+      .save(routeId, route)
+      .doOnNext(response -> this.gatewayControllerEndpoint.refresh())
+      .map(response -> ResponseEntity.status(response.getStatusCode()).build());
+  }
+
+  @DeleteMapping("/{routeId}")
+  public Mono<ResponseEntity<Object>> deleteRoute(@PathVariable String routeId) {
+    return this.gatewayControllerEndpoint
+      .delete(routeId)
+      .doOnNext(response -> this.gatewayControllerEndpoint.refresh());
   }
 }
---
+++ src/test/groovy/com/ardalo/digitalplatform/gateway/routing/RouteControllerIT.groovy
@@ -0,0 +1,131 @@
+package com.ardalo.digitalplatform.gateway.routing
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import spock.lang.Specification
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class RouteControllerIT extends Specification {
+
+  @Autowired
+  ApplicationContext applicationContext
+
+  WebTestClient webTestClient
+
+  def setup() {
+    this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build()
+  }
+
+  def "should return existing route"() {
+    given:
+    webTestClient
+      .post()
+      .uri("/api/routes/v1/test-route-003")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue('{"uri":"http://backend-service:80/test","predicates":[{"name":"Path","args":{"arg0":"/test"}}]}')
+      .exchange()
+      .expectStatus().isCreated()
+
+    expect:
+    webTestClient
+      .get()
+      .uri("/api/routes/v1/test-route-003")
+      .exchange()
+      .expectStatus().isOk()
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody().json('{"route_id":"test-route-003","predicate":"Paths: [/test], match trailing slash: true","filters":[],"uri":"http://backend-service:80/test","order":0}')
+
+    cleanup:
+    webTestClient
+      .delete()
+      .uri("/api/routes/v1/test-route-003")
+      .exchange()
+      .expectStatus().isOk()
+  }
+
+  def "should return 404 Not Found when querying non existing route"() {
+    expect:
+    webTestClient
+      .get()
+      .uri("/api/routes/v1/bdibdjhsbcvshbc")
+      .exchange()
+      .expectStatus().isNotFound()
+      .expectBody().isEmpty()
+  }
+
+  def "should return empty list of routes"() {
+    expect:
+    webTestClient
+      .get()
+      .uri("/api/routes/v1")
+      .exchange()
+      .expectStatus().isOk()
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody().json("[]")
+  }
+
+  def "should return existing routes"() {
+    given:
+    webTestClient
+      .post()
+      .uri("/api/routes/v1/test-route-001")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue('{"id":"foobar","uri":"http://backend-service:80/test","predicates":[{"name":"Path","args":{"arg0":"/test"}}]}')
+      .exchange()
+      .expectStatus().isCreated()
+
+    expect:
+    webTestClient
+      .get()
+      .uri("/api/routes/v1")
+      .exchange()
+      .expectStatus().isOk()
+      .expectHeader().contentType(MediaType.APPLICATION_JSON)
+      .expectBody().json('[{"route_id":"test-route-001","predicate":"Paths: [/test], match trailing slash: true","filters":[],"uri":"http://backend-service:80/test","order":0}]')
+
+    cleanup:
+    webTestClient
+      .delete()
+      .uri("/api/routes/v1/test-route-001")
+      .exchange()
+      .expectStatus().isOk()
+  }
+
+  def "should create route"() {
+    expect:
+    webTestClient
+      .post()
+      .uri("/api/routes/v1/test-route-002")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue('{"id":"foobar","uri":"http://backend-service:80/test","predicates":[{"name":"Path","args":{"arg0":"/test"}}]}')
+      .exchange()
+      .expectStatus().isCreated()
+      .expectHeader().doesNotExist(HttpHeaders.LOCATION)
+      .expectBody().isEmpty()
+
+    cleanup:
+    webTestClient
+      .delete()
+      .uri("/api/routes/v1/test-route-002")
+      .exchange()
+      .expectStatus().isOk()
+  }
+
+  def "should return 400 Bad Request when trying to create route with invalid format"() {
+    expect:
+    webTestClient
+      .post()
+      .uri("/api/routes/v1/test-route-005")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue('{"uri":"http://backend-service:80/test","predicates":[{"name":"Unknown"}]}')
+      .exchange()
+      .expectStatus().isBadRequest()
+      .expectBody().isEmpty()
+  }
+}
