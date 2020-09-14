3cc15232ec1960ecee38ab601d894821c8ebdb8f
Torsten Blasche <torsten.blasche@obi.de>
2020-09-14 12:07:46 +0200

    Make gateway work

--- src/main/java/com/ardalo/digitalplatform/gateway/PlatformGatewayApplication.java
+++ src/main/java/com/ardalo/digitalplatform/gateway/PlatformGatewayApplication.java
@@ -1,7 +1,15 @@
 package com.ardalo.digitalplatform.gateway;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.actuate.GatewayControllerEndpoint;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.cloud.gateway.handler.predicate.RoutePredicateFactory;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PlatformGatewayApplication {
@@ -9,4 +17,21 @@ public class PlatformGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformGatewayApplication.class, args);
 	}
+
+  /**
+   * Copy of {@link org.springframework.cloud.gateway.config.GatewayAutoConfiguration.GatewayActuatorConfiguration#gatewayControllerEndpoint(List, List, List, RouteDefinitionWriter, RouteLocator)} gatewayControllerEndpoint}.
+   *
+   * Necessary because this bean is not being initialized via auto configuration as the
+   * Spring Cloud Gateway Actuator Endpoint is disabled. This is done to have full control
+   * of the endpoints exposed by this Gateway.
+   */
+  @Bean
+  public GatewayControllerEndpoint gatewayControllerEndpoint(
+    List<GlobalFilter> globalFilters,
+    List<GatewayFilterFactory> gatewayFilters,
+    List<RoutePredicateFactory> routePredicates,
+    RouteDefinitionWriter routeDefinitionWriter, RouteLocator routeLocator) {
+    return new GatewayControllerEndpoint(globalFilters, gatewayFilters,
+                                         routePredicates, routeDefinitionWriter, routeLocator);
+  }
 }
