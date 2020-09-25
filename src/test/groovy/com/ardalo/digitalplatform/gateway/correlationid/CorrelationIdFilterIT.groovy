a172520c802e05c7e36d281eade17a76e66bdd47
Torsten Blasche <torsten.blasche@obi.de>
2020-09-25 22:22:20 +0200

    Added integration test for CorrelationIdFilter

---
+++ src/test/groovy/com/ardalo/digitalplatform/gateway/correlationid/CorrelationIdFilterIT.groovy
@@ -0,0 +1,60 @@
+package com.ardalo.digitalplatform.gateway.correlationid
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import spock.lang.Specification
+
+import java.util.concurrent.TimeUnit
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class CorrelationIdFilterIT extends Specification {
+
+  @Autowired
+  ApplicationContext applicationContext
+
+  WebTestClient webTestClient
+  MockWebServer mockWebServer
+
+  def setup() {
+    this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build()
+    this.mockWebServer = new MockWebServer()
+    this.mockWebServer.start()
+    this.mockWebServer.enqueue(new MockResponse())
+  }
+
+  def cleanup() {
+    this.mockWebServer.shutdown()
+  }
+
+  def "should add X-Correlation-ID header to downstream requests"() {
+    given: "an existing platform route"
+    webTestClient
+      .post()
+      .uri("/gateway/api/routes/v1/correlation-id-test")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue('{"uri":"http://' + this.mockWebServer.hostName + ':' + this.mockWebServer.port + '","predicates":[{"name":"Path","args":{"arg0":"/correlation-id-test"}}]}')
+      .exchange()
+      .expectStatus().isCreated()
+
+    when: "existing platform route is called"
+    webTestClient
+      .get()
+      .uri("/correlation-id-test")
+      .exchange()
+      .expectStatus().isOk()
+
+    then: "routed request contains X-Correlation-ID request header"
+    def recordedRequest = mockWebServer.takeRequest(50, TimeUnit.MILLISECONDS)
+    recordedRequest.method == HttpMethod.GET.toString()
+    recordedRequest.path == "/correlation-id-test"
+    recordedRequest.getHeader("X-Correlation-ID").matches('^[a-f0-9-]+$')
+  }
+}
